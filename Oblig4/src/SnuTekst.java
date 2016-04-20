477798f3eb3e96c6fa8c676da511aa5f41ef881e
Andreas Paulsen <Andreas Paulsen@Andreas>
2016-04-20 13:42:39 +0200

    v0.2

--- Oblig4/src/SnuTekst.java
+++ Oblig4/src/SnuTekst.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class SnuTekst {
@@ -17,6 +19,19 @@ public class SnuTekst {
 			System.out.println("Skriv inn en streng: ");
 			String tekst = input.nextLine();
 			baklengs(tekst);
+
+			int count = tekst.length();
+			Map<Character, Integer> numChars = new HashMap<Character, Integer>(Math.min(count, 26));
+
+			for (int i = 0; i < count; ++i) {
+				char charAt = tekst.charAt(i);
+				if (!numChars.containsKey(charAt)) {
+					numChars.put(charAt, 1);
+				} else {
+					numChars.put(charAt, numChars.get(charAt) + 1);
+				}
+			}
+			System.out.println(numChars);
 		}
 		input.close();
 	}

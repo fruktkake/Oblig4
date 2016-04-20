9c98f16c0533af733c206e9b8625372eac4b81c2
Andreas Paulsen <Andreas Paulsen@Andreas>
2016-04-20 14:24:35 +0200

    v0.2

--- Oblig4/src/TowerOfHanoi.java
+++ Oblig4/src/TowerOfHanoi.java
@@ -5,26 +5,29 @@ public class TowerOfHanoi {
   public static void main(String[] args) {
     // Create a Scanner
     Scanner input = new Scanner(System.in);
-    System.out.print("Enter number of disks: ");
+    System.out.print("Skriv inn antall skiver: ");
     int n = input.nextInt();
-    int NUMBER_OF_MOVES;
 
     // Find the solution recursively
-    System.out.println("The moves are:");
+    System.out.println("Trekkene er:");
     moveDisks(n, 'A', 'B', 'C');
+    System.out.println("\nMed " + n + " skiver ble løsningen fullført med " + count + " trekk!");
   }
+  
+  static int count;
 
   /** The method for finding the solution to move n disks
       from fromTower to toTower with auxTower */
   public static void moveDisks(int n, char fromTower,
       char toTower, char auxTower) {
+	  count++;
     if (n == 1) // Stopping condition
-      System.out.println("Move disk " + n + " from " +
-        fromTower + " to " + toTower);
+      System.out.println("Flytt skive " + n + " fra " +
+        fromTower + " til " + toTower);
     else {
       moveDisks(n - 1, fromTower, auxTower, toTower);
-      System.out.println("Move disk " + n + " from " +
-        fromTower + " to " + toTower);
+      System.out.println("Flytt skive " + n + " fra " +
+        fromTower + " til " + toTower);
       moveDisks(n - 1, auxTower, toTower, fromTower);
     }
   }
